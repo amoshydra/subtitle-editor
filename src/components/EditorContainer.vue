add6859b99dd9241a2415dcf0951858ce5816500
Amos Wong <amoswwj@gmail.com>
2019-12-07 21:57:05 +0800

    chore: Initialize project

---
+++ src/components/EditorContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+
+</style>
